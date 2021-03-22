adc867f482cec5c714de404fa127cbfa62024130
Adam Brzosko <47273267+ab7018@users.noreply.github.com>
2021-03-22 18:06:07 +0000

    errorbars and start of .4 analysis

--- Project.py
+++ Project.py
@@ -13,7 +13,7 @@ import collections
 from scipy import stats
 #import random
 #import os 
-outputdir = '/home/adam/Desktop/work/3rd Year/Networks/Project' 
+outputdir = '/home/adam/Desktop/work/3rd Year/Networks/ProjectData' 
 
 
 StartingSize = 64
@@ -119,7 +119,7 @@ def DriveMixed(s = 3, q=2/3):
         AddEdgesPref(m = s)
 
 
-def logbin(data, scale = 1., MaxDeg = 0, zeros = True):
+def logbin(data, scale = 1., MaxDeg = 0, MinDeg = 2, zeros = True):
     """
     Taken from:
     Max Falkenberg McGillivray
@@ -136,9 +136,10 @@ def logbin(data, scale = 1., MaxDeg = 0, zeros = True):
         smax = np.max(data)
     if scale > 1:
         jmax = np.ceil(np.log(smax)/np.log(scale))
+        jmin = np.floor(np.log(MinDeg)/np.log(scale))
         if zeros:
-            binedges = scale ** np.arange(jmax + 1)
-            binedges[0] = 0
+            binedges = scale ** np.arange(jmin, jmax + 1)  #was just jmax+1
+            binedges[0] = MinDeg                #was 0
         else:
             binedges = scale ** np.arange(1,jmax + 1)
             # count = count[1:]
@@ -161,6 +162,14 @@ def logbin(data, scale = 1., MaxDeg = 0, zeros = True):
 
 def DegDist(x, m=1):
     p = 2*(m)*(m+1)/(x*(x+1)*(x+2))
+    return p
+
+def DegDistRand(x,m=1):
+    p = (m**(x-m))/((1+m)**(1+x-m))
+    return p
+
+def DegDistMixed(x, m=1):
+    p = 1
     return p
 
 #%%
@@ -187,47 +196,24 @@ for it in range(1,11):
 
 
         for i in range(100000):
-            DrivePref(r)
-        with open(outputdir+'/1.3Final/deg100k-M{}Run{}'.format(r,it), 'wb') as f:
+            DriveMixed(r)
+        with open(outputdir+'/3.3Final/deg100k-M{}Run{}'.format(r,it), 'wb') as f:
             pickle.dump(weights,f)
-        with open(outputdir+'/1.3Final/net100k-M{}Run{}'.format(r,it), 'wb') as f:
+        with open(outputdir+'/3.3Final/net100k-M{}Run{}'.format(r,it), 'wb') as f:
             pickle.dump(network,f)      
         
-#%%
-'''
-task 1.3
-'''
-
-C = globals()
-for k in range(1,6):
-    with open(outputdir+'/deg100k-M{}Run4'.format(k), 'rb') as f:
-         C['C{}'.format(k)] = sorted(collections.Counter(pickle.load(f)).items())
-    x, y = zip(*C['C{}'.format(k)])
-    x = np.array(x)
-    total = sum(y)
-    y = np.array([i/total for i in y])
-    plt.plot(x,y, label = 'M = {}'.format(k))
-    
-    test_stat = stats.ks_2samp(y, DegDist(x, m=k))
-    print(test_stat) #too few data points for KS??
-    slope, intercept, r_value, p_value, std_err = stats.linregress(y, DegDist(x, m=k))
-    print(slope, intercept, r_value, p_value, std_err)
-    
-plt.yscale('log')
-plt.xscale('log')
-plt.legend()
-plt.show()
    
 #%%
 '''
-logbinned
+1.3 analysis
 '''
+
 Data = {}
 for k in range(1,6):
     L = 2**k
     Maxes = []
-    for it in range(2,10):
-        with open(outputdir+'/1.3RandomInitial100k/deg100k-M{}Run{}'.format(L,it), 'rb') as f:
+    for it in range(1,11):
+        with open(outputdir+'/1.3Final/deg100k-M{}Run{}'.format(L,it), 'rb') as f:
             Data['M{}Run{}'.format(L,it)] = pickle.load(f)
             Maxes.append(np.max(Data['M{}Run{}'.format(L,it)]))
     Data['KmaxM{}'.format(L)] = np.max(Maxes)
@@ -237,8 +223,8 @@ for k in range(1,6):
     Data['X{}'.format(L)] = []
     Data['Y{}'.format(L)] = []
     Biggest = 0
-    for it in range(2,10):
-        Data['X{}Run{}'.format(L,it)], Data['Y{}Run{}'.format(L,it)] = logbin(Data['M{}Run{}'.format(L,it)], 1.1, Data['KmaxM{}'.format(L)])
+    for it in range(1,11):
+        Data['X{}Run{}'.format(L,it)], Data['Y{}Run{}'.format(L,it)] = logbin(Data['M{}Run{}'.format(L,it)], 1.1, Data['KmaxM{}'.format(L)], L)
        
         for x in Data['X{}Run{}'.format(L,it)]:
             if x not in Data['X{}'.format(L)]:
@@ -246,13 +232,30 @@ for k in range(1,6):
                 Data['Y{}'.format(L)].append(Data['Y{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)])
             else:
                 Data['Y{}'.format(L)][Data['X{}'.format(L)].index(x)] += Data['Y{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)]
-                
-
+    Data['Y{}'.format(L)] = [x/10 for x in Data['Y{}'.format(L)]]
+    Data['Y{}Var'.format(L)] = [0 for x in Data['X{}'.format(L)]]
+    Data['Y{}Read'.format(L)] = [0 for x in Data['X{}'.format(L)]]
+    for it in range(1,11):
+        for x in Data['X{}Run{}'.format(L,it)]:
+            Data['Y{}Var'.format(L)][Data['X{}'.format(L)].index(x)] +=  (Data['Y{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)] - Data['Y{}'.format(L)][Data['X{}'.format(L)].index(x)])**2
+            Data['Y{}Read'.format(L)][Data['X{}'.format(L)].index(x)] += 1
+    Data['Y{}Stdev'.format(L)] = [np.sqrt(Data['Y{}Var'.format(L)][i]/Data['Y{}Read'.format(L)][i]) for i in range(len(Data['Y{}Var'.format(L)]))]
+    Data['X{}'.format(L)], Data['Y{}'.format(L)], Data['Y{}Stdev'.format(L)] = zip(*sorted(zip(Data['X{}'.format(L)], Data['Y{}'.format(L)], Data['Y{}Stdev'.format(L)])))
+    Data['X{}'.format(L)] = np.array((Data['X{}'.format(L)]))
+    Data['Y{}'.format(L)] = np.array((Data['Y{}'.format(L)]))
+    Data['Y{}Stdev'.format(L)] = np.array((Data['Y{}Stdev'.format(L)]))
 for k in range(1,6):
+    base = np.linspace(k**2,60*k,1000)
+    #base = np.linspace(k**2,3000,1000) #preferential
     L = 2**k
-    Data['Y{}'.format(L)] = [x/9 for x in Data['Y{}'.format(L)]]
-    plt.plot(Data['X{}'.format(L)],Data['Y{}'.format(L)], '+', label = 'M = {}'.format(L))
+    plt.errorbar(Data['X{}'.format(L)],Data['Y{}'.format(L)], yerr = Data['Y{}Stdev'.format(L)], label = 'M = {}'.format(L), capsize = 3)
+    #plt.plot(base, DegDistRand(base,L), '-', label = 'Theoretical M = {}'.format(L))
 
+    test_stat = stats.ks_2samp(Data['Y{}'.format(L)], DegDist(Data['X{}'.format(L)], m=L))
+    print(test_stat)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(Data['Y{}'.format(L)], DegDist(Data['X{}'.format(L)], m=L))
+    print(slope, intercept, r_value, p_value, std_err)
+    
 plt.yscale('log')
 plt.xscale('log')
 plt.legend()
@@ -261,17 +264,15 @@ plt.show()
 #%%
 '''
 1.4 data
-we go to highest m possible, m=5
+we go to smalles m possible, m=2
 '''
-for j in range(10,18):
-    StartingSize = 32
-    network = [[] for i in range(StartingSize)]
-    for i in range(StartingSize):
-        att = np.random.choice(StartingSize, np.random.choice(np.arange(1,StartingSize)), replace = False)
-        for k in att:
-            if i not in network[k] and i!=k:
-                network[k].append(i)
-                network[i].append(k)
+for it in range(1,11):
+    for j in range(10,18):
+        network = [[] for i in range(2)]
+        for i in range(2):
+            for k in range(2):
+                if i!=k:    
+                    network[i].append(k)
     weights = []                            #how many edges each vertex has
     X = len(network)                        #size of initial network
     T = 0                                   #time
@@ -283,9 +284,22 @@ for j in range(10,18):
         P.append(weights[i]/Weight)         #probabilities of assignment (preferential)
     
     for k in range(2**j): #do like power of 2 or something
-        DriveRandom(s=2)
-    with open(outputdir+'/MixdegM2-N2e{}'.format(j), 'wb') as f:
+        DrivePref(s=2)
+    with open(outputdir+'/1.4Final/degM2-N2e{}Run{}'.format(j,it), 'wb') as f:
         pickle.dump(weights,f)
-    with open(outputdir+'/MixdnetM2-N2e{}'.format(j), 'wb') as f:
+    with open(outputdir+'/1.4Final/netM2-N2e{}Run{}'.format(j,it), 'wb') as f:
         pickle.dump(network,f)     
 
+#%%
+'''
+1.4 analysis
+'''
+DataK = {}
+for k in range(10,18):
+    N = 2**j
+    Maxes = []
+    for it in range(1,11):
+        with open(outputdir+'/1.4Final/degM2-N2e{}Run{}'.format(N,it), 'rb') as f:
+            DataK['M{}Run{}'.format(L,it)] = pickle.load(f)
+            Maxes.append(np.max(DataK['M{}Run{}'.format(L,it)]))
+    DataK['KmaxM{}'.format(L)] = np.max(Maxes)
