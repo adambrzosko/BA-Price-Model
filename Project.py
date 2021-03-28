4e42d65e54bf0d91170237c5c34b8de551c00c87
Adam Brzosko <47273267+ab7018@users.noreply.github.com>
2021-03-28 17:58:02 +0100

    fixed tests

--- Project.py
+++ Project.py
@@ -15,6 +15,7 @@ from scipy.optimize import curve_fit as cf
 #import random
 #import os 
 outputdir = '/home/adam/Desktop/work/3rd Year/Networks/ProjectData' 
+graphdir = '/home/adam/Desktop/work/3rd Year/Networks/Report'
 
 
 StartingSize = 64
@@ -141,38 +142,61 @@ def logbin(data, scale = 1., MaxDeg = 0, MinDeg = 2, zeros = True):
         binedges = np.unique(binedges.astype('uint64'))
         x = (binedges[:-1] * (binedges[1:]-1)) ** 0.5
         y = np.zeros_like(x)
+        stdev = np.zeros_like(x)
+        number = np.zeros_like(x)
         count = count.astype('float')
         for i in range(len(y)):
             y[i] = np.sum(count[binedges[i]:binedges[i+1]]/(binedges[i+1] - binedges[i]))
+            number[i] = np.sum(count[binedges[i]:binedges[i+1]]) #number of avalnches in bin i
+#        y /= tot
+#        for i in range(len(y)):
+#            probs = []
+#            for j in range(binedges[i]-1,binedges[i+1]-1):
+#                probs.append((y[i]-(count[j]/tot))**2)
+#            stdev[i] = np.sqrt(sum(probs)/(binedges[i+1] - binedges[i]))
     else:
         x = np.nonzero(count)[0]
         y = count[count != 0].astype('float')
         if zeros != True and x[0] == 0:
             x = x[1:]
             y = y[1:]
+#        y /= tot
+#        for i in range(len(y)):
+#            for j in range(binedges[i],binedges[i+1]):
+#                stdev[i] = np.sum((y[i]-(count[j]/tot))**2)/(binedges[i+1] - binedges[i])
     y /= tot
     x = x[y!=0]
     y = y[y!=0]
-    return x,y
+#    stdev = stdev[stdev!=0]
+    return x,y,number
 
 def DegDist(x, m=1):
     p = 2*(m)*(m+1)/(x*(x+1)*(x+2))
     return p
 
 def DegDistRand(x,m=1):
-    p = (m**(x-m))/((1+m)**(1+x-m))
+    #p = (m**(x-m))/((1+m)**(1+x-m))
+    p = ((m/(1+m))**(x-m))*(1/(1+m))
     return p
 
-def DegDistMixed(x, m=1):
-    p = 1
+def DegDistMix12(x, m=1):
+    A = 12*m*(3*m+1)*(3*m+2)*(3*m+3)
+    B = (k+2*m)*(k+2*m+1)(k+2*m+2)(k+2*m+3)(k+2*m+4)
+    p = A/B
+    return p
+
+def DegDistMix23(x, m=1):
+    A = 6*m*(2*m+1)*(2*m+2)
+    B = (k+m)*(k+m+1)(k+m+2)(k+m+3)
+    p = A/B
     return p
 
 #%%
 '''
 task 1.3 data
 '''
-for it in range(11,101):
-    for r in [2, 4, 8, 16, 32]:
+for it in range(101,201):
+    for r in [2, 4, 8, 16, 32, 64]:
         network = [[] for i in range(r)]
         for i in range(r):
             for k in range(r):
@@ -189,8 +213,8 @@ for it in range(11,101):
         Weight = sum(weights)                   
 
         for i in range(100000):
-            DriveRandom(r)
-        with open(outputdir+'/2.3Final/deg100k-M{}Run{}'.format(r,it), 'wb') as f:
+            DriveMixed(r,0.5)
+        with open(outputdir+'/3.3Final12/deg100k-M{}Run{}'.format(r,it), 'wb') as f:
             pickle.dump(weights,f)
 #        with open(outputdir+'/1.3Final/net100k-M{}Run{}'.format(r,it), 'wb') as f:
 #            pickle.dump(network,f)      
@@ -200,65 +224,107 @@ for it in range(11,101):
 '''
 1.3 analysis
 '''
-
-Data = {}
-for k in range(1,6):
+colors = ['magenta', 'firebrick', 'royalblue', 'orangered', 'black', 'forestgreen', 'navy']
+Data = {}    #dictionary for all the data
+for k in range(1,7):
     L = 2**k
-    Maxes = []
-    for it in range(31,51):
+    Maxes = []    #list of max degrees in every run
+#    Data['All{}'.format(L)] = [] #a list for all degrees from all runs
+    for it in range(1,201):
         with open(outputdir+'/1.3Final/deg100k-M{}Run{}'.format(L,it), 'rb') as f:
-            Data['M{}Run{}'.format(L,it)] = pickle.load(f)
-            Maxes.append(np.max(Data['M{}Run{}'.format(L,it)]))
-    Data['KmaxM{}'.format(L)] = np.max(Maxes)
-    
-for k in range(1,6):
+            Data['M{}Run{}'.format(L,it)] = pickle.load(f)   #load all runs with different ms
+            Maxes.append(np.max(Data['M{}Run{}'.format(L,it)]))  #find max degree for every run and every m
+#            Data['All{}'.format(L)] += Data['M{}Run{}'.format(L,it)]
+    Data['KmaxM{}'.format(L)] = np.max(Maxes)  #get the ultimate max degree for every m
+#    Data['Xall{}'.format(L)], Data['Yall{}'.format(L)], Data['Numall{}'.format(L)] = logbin(Data['All{}'.format(L)], 1.3, Data['KmaxM{}'.format(L)], L)
+for k in range(1,7):
     L = 2**k
     Data['X{}'.format(L)] = []
     Data['Y{}'.format(L)] = []
-    for it in range(31,51):
-        Data['X{}Run{}'.format(L,it)], Data['Y{}Run{}'.format(L,it)] = logbin(Data['M{}Run{}'.format(L,it)], 1.1, Data['KmaxM{}'.format(L)], L)
+    Data['Number{}'.format(L)] = []
+#    Data['Dev{}'.format(L)] = []
+#    Data['YallStdev{}'.format(L)] = [0 for x in Data['Xall{}'.format(L)]]
+#    Data['YallReads{}'.format(L)] = [0 for x in Data['Xall{}'.format(L)]]
+    for it in range(1,201): #logbin the data for each run and each m
+        Data['X{}Run{}'.format(L,it)], Data['Y{}Run{}'.format(L,it)], Data['Number{}Run{}'.format(L,it)] = logbin(Data['M{}Run{}'.format(L,it)], 1.3, Data['KmaxM{}'.format(L)], L)
        
-        for x in Data['X{}Run{}'.format(L,it)]:
-            if x not in Data['X{}'.format(L)]:
-                Data['X{}'.format(L)].append(x)
+        for x in Data['X{}Run{}'.format(L,it)]:  #run through all runs of different m 
+            if x not in Data['X{}'.format(L)]:   #make sure the master array for each m has all the bins
+                Data['X{}'.format(L)].append(x)  #append their probabilities below if the master doesn't have that entry
                 Data['Y{}'.format(L)].append(Data['Y{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)])
-            else:
+                Data['Number{}'.format(L)].append(Data['Number{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)])
+            else: #increase the probabilities in the master by the probs of the run if the bins already are there
                 Data['Y{}'.format(L)][Data['X{}'.format(L)].index(x)] += Data['Y{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)]
-    Data['Y{}'.format(L)] = [x/30 for x in Data['Y{}'.format(L)]]
-    Data['Y{}Var'.format(L)] = [0 for x in Data['X{}'.format(L)]]
-    Data['Y{}Read'.format(L)] = [0 for x in Data['X{}'.format(L)]]
-    for it in range(31,51):
-        for x in Data['X{}Run{}'.format(L,it)]:
-            Data['Y{}Var'.format(L)][Data['X{}'.format(L)].index(x)] +=  (Data['Y{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)] - Data['Y{}'.format(L)][Data['X{}'.format(L)].index(x)])**2
-            Data['Y{}Read'.format(L)][Data['X{}'.format(L)].index(x)] += 1
+                Data['Number{}'.format(L)][Data['X{}'.format(L)].index(x)] += Data['Number{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)]
+#                Data['Dev{}'.format(L)][Data['X{}'.format(L)].index(x)] += Data['Dev{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)]
+#        for stdev in Data['X{}Run{}'.format(L,it)]:
+#            Data['YallStdev{}'.format(L)][np.ndarray.tolist(Data['Xall{}'.format(L)]).index(stdev)] += (Data['Y{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(stdev)] - Data['Yall{}'.format(L)][np.ndarray.tolist(Data['Xall{}'.format(L)]).index(stdev)])**2
+#            Data['YallReads{}'.format(L)][np.ndarray.tolist(Data['Xall{}'.format(L)]).index(stdev)] += 1
+#    Data['Numpy{}'.format(L)] = [[] for i in Data['X{}'.format(L)]]
+    Data['Y{}'.format(L)] = [x/200 for x in Data['Y{}'.format(L)]] #divided by the number of runs
+    Data['Y{}Var'.format(L)] = [0 for x in Data['X{}'.format(L)]] #for error calculation
+    Data['Y{}Read'.format(L)] = [0 for x in Data['X{}'.format(L)]] #also for errors
+    for it in range(1,201):
+        for x in Data['X{}Run{}'.format(L,it)]: #(below) append the variances in the right places from all the runs
+            Data['Y{}Var'.format(L)][Data['X{}'.format(L)].index(x)] += (Data['Y{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)] - Data['Y{}'.format(L)][Data['X{}'.format(L)].index(x)])**2
+            Data['Y{}Read'.format(L)][Data['X{}'.format(L)].index(x)] += 1  #if variance appended, increase the count N
+#            Data['Numpy{}'.format(L)][Data['X{}'.format(L)].index(x)].append(Data['Y{}Run{}'.format(L,it)][np.ndarray.tolist(Data['X{}Run{}'.format(L,it)]).index(x)])
     Data['Y{}Stdev'.format(L)] = [np.sqrt(Data['Y{}Var'.format(L)][i]/Data['Y{}Read'.format(L)][i]) for i in range(len(Data['Y{}Var'.format(L)]))]
     Data['X{}'.format(L)], Data['Y{}'.format(L)], Data['Y{}Stdev'.format(L)] = zip(*sorted(zip(Data['X{}'.format(L)], Data['Y{}'.format(L)], Data['Y{}Stdev'.format(L)])))
+#    Data['Error{}'.format(L)] = [np.std(Data['Numpy{}'.format(L)][i]) for i in range(len(Data['Y{}Var'.format(L)]))]
+#    Data['Y{}Error'.format(L)] = [np.sqrt(Data['YallStdev{}'.format(L)][i]/Data['YallReads{}'.format(L)][i]) for i in range(len(Data['YallStdev{}'.format(L)]))]
+#    Data['Y{}Error'.format(L)] = np.array(Data['Y{}Error'.format(L)])
     Data['X{}'.format(L)] = np.array((Data['X{}'.format(L)]))
     Data['Y{}'.format(L)] = np.array((Data['Y{}'.format(L)]))
-    Data['Y{}Stdev'.format(L)] = np.array((Data['Y{}Stdev'.format(L)]))
-for k in range(1,6):
-    base = np.linspace(k**2,60*k,1000)
-    #base = np.linspace(k**2,3000,1000) #preferential
+    Data['Y{}Stdev'.format(L)] = np.array(Data['Y{}Stdev'.format(L)])
+for k in range(1,7):
+    #base = np.linspace(k**2,60*k,1000) #random
+    base = np.linspace(2**k-0.5*k,4800,10000) #preferential
+    #base = np.float128(base)
     L = 2**k
-    plt.errorbar(Data['X{}'.format(L)],Data['Y{}'.format(L)], yerr = Data['Y{}Stdev'.format(L)], label = 'M = {}'.format(L), capsize = 3)
-    #plt.plot(base, DegDistRand(base,L), '-', label = 'Theoretical M = {}'.format(L))
-
+    plt.plot(base, DegDist(base,L), '--', color = colors[k-1])
+    plt.errorbar(Data['X{}'.format(L)],Data['Y{}'.format(L)], yerr = Data['Y{}Stdev'.format(L)], label = '$m = {}$'.format(L), capsize = 3, color = colors[k-1])
+    
+plt.yscale('log')
+plt.xscale('log')
+plt.legend(fontsize = 12, loc = 'lower left')
+plt.xlabel(r'$k$', fontsize = 15)
+plt.ylabel(r'$p(k)$', fontsize = 15)
+plt.savefig(graphdir+'/13Pref', dpi=500)
+#%%
+'''
+1.3 tests
+'''
+def chisqr_reduced(obs, exp, error, reads):
+    chisqr_reduced = 0
+    m = max(reads)
+    #print(reads)
+    for i in range(len(obs)):
+        chisqr = ((obs[i]-exp[i])**2)/(error[i])
+        chisqr_reduced += chisqr/(m+1-reads[i])
+    return chisqr_reduced
+for k in range(1,7):
+    #base = np.linspace(k**2,60*k,1000) #random
+    base = np.linspace(2**k-0.5*k,4800,10000) #preferential
+    #base = np.float128(base)
+    b = 8-k  #[:-b]
+    L = 2**k
+    observed = [Data['Y{}'.format(L)][i]*Data['Number{}'.format(L)][i] for i in range(len(Data['Y{}'.format(L)]))]
+    expected = [DegDist(Data['X{}'.format(L)][i], m=L)*Data['Number{}'.format(L)][i] for i in range(len(Data['Y{}'.format(L)]))]
+    error = [DegDist(Data['Y{}Stdev'.format(L)][i], m=L)*Data['Number{}'.format(L)][i] for i in range(len(Data['Y{}'.format(L)]))]
     test_stat = stats.ks_2samp(Data['Y{}'.format(L)], DegDist(Data['X{}'.format(L)], m=L))
     print(test_stat)
     slope, intercept, r_value, p_value, std_err = stats.linregress(Data['Y{}'.format(L)], DegDist(Data['X{}'.format(L)], m=L))
     print(slope, intercept, r_value, p_value, std_err)
-    
-plt.yscale('log')
-plt.xscale('log')
-plt.legend()
-plt.show()
-
+    chi = chisqr_reduced(observed[:-b], expected[:-b], error[:-b], Data['Y{}Read'.format(L)][:-b])
+#    chi = stats.chisquare(Data['Y{}'.format(L)], DegDist(Data['X{}'.format(L)], m=L))
+#    print('M =', L, 'chi=', chi, 'p=', stats.distributions.chi2.sf(chi,1))
 #%%
 '''
 1.4 data
 we go to smalles m possible, m=2
 '''
-for it in range(21,101):
+for it in range(101,201):
     for j in range(10,18):
         network = [[] for i in range(2)]
         for i in range(2):
@@ -277,8 +343,8 @@ for it in range(21,101):
         
     
         for k in range(2**j): #do like power of 2 or something
-            DrivePref(s=2)
-        with open(outputdir+'/1.4Final/degM2-N2e{}Run{}'.format(j,it), 'wb') as f:
+            DriveMixed(2)
+        with open(outputdir+'/3.4Final/degM2-N2e{}Run{}'.format(j,it), 'wb') as f:
             pickle.dump(weights,f)
 #        with open(outputdir+'/1.4Final/netM2-N2e{}Run{}'.format(j,it), 'wb') as f:
 #            pickle.dump(network,f)     
@@ -291,7 +357,7 @@ DataK = {}
 for j in range(10,18):
     N = j
     Maxes = []
-    for it in range(1,101):
+    for it in range(1,201):
         with open(outputdir+'/1.4Final/degM2-N2e{}Run{}'.format(N,it), 'rb') as f:
             DataK['N{}Run{}'.format(N,it)] = pickle.load(f)
             Maxes.append(np.max(DataK['N{}Run{}'.format(N,it)]))
@@ -302,8 +368,8 @@ for k in range(10,18):
     N = k
     DataK['X{}'.format(N)] = []
     DataK['Y{}'.format(N)] = []
-    for it in range(1,101):
-        DataK['X{}Run{}'.format(N,it)], DataK['Y{}Run{}'.format(N,it)] = logbin(DataK['N{}Run{}'.format(N,it)], 1.3, DataK['KmaxN{}'.format(N)], 2)
+    for it in range(1,201):
+        DataK['X{}Run{}'.format(N,it)], DataK['Y{}Run{}'.format(N,it)], DataK['Number{}Run{}'.format(N,it)] = logbin(DataK['N{}Run{}'.format(N,it)], 1.3, DataK['KmaxN{}'.format(N)], 2)
        
         for x in DataK['X{}Run{}'.format(N,it)]:
             if x not in DataK['X{}'.format(N)]:
@@ -311,10 +377,10 @@ for k in range(10,18):
                 DataK['Y{}'.format(N)].append(DataK['Y{}Run{}'.format(N,it)][np.ndarray.tolist(DataK['X{}Run{}'.format(N,it)]).index(x)])
             else:
                 DataK['Y{}'.format(N)][DataK['X{}'.format(N)].index(x)] += DataK['Y{}Run{}'.format(N,it)][np.ndarray.tolist(DataK['X{}Run{}'.format(N,it)]).index(x)]
-    DataK['Y{}'.format(N)] = [x/100 for x in DataK['Y{}'.format(N)]]
+    DataK['Y{}'.format(N)] = [x/200 for x in DataK['Y{}'.format(N)]]
     DataK['Y{}Var'.format(N)] = [0 for x in DataK['X{}'.format(N)]]
     DataK['Y{}Read'.format(N)] = [0 for x in DataK['X{}'.format(N)]]
-    for it in range(1,101):
+    for it in range(1,201):
         for x in DataK['X{}Run{}'.format(N,it)]:
             DataK['Y{}Var'.format(N)][DataK['X{}'.format(N)].index(x)] +=  (DataK['Y{}Run{}'.format(N,it)][np.ndarray.tolist(DataK['X{}Run{}'.format(N,it)]).index(x)] - DataK['Y{}'.format(N)][DataK['X{}'.format(N)].index(x)])**2
             DataK['Y{}Read'.format(N)][DataK['X{}'.format(N)].index(x)] += 1
@@ -327,12 +393,14 @@ for k in range(10,18):
     base = np.linspace(k**2,60*k,1000)
     #base = np.linspace(k**2,3000,1000) #preferential
     N = k
-    plt.errorbar(DataK['X{}'.format(N)],DataK['Y{}'.format(N)], yerr = DataK['Y{}Stdev'.format(N)], label = 'M = {}'.format(N), capsize = 3)
+    plt.errorbar(DataK['X{}'.format(N)],DataK['Y{}'.format(N)], yerr = DataK['Y{}Stdev'.format(N)], label = 'N = 2^{}'.format(N), capsize = 3)
     #plt.plot(base, DegDistRand(base,L), '-', label = 'Theoretical M = {}'.format(L))
 plt.yscale('log')
 plt.xscale('log')
-plt.legend()
-plt.show()
+plt.legend(fontsize = 12, loc = 'lower left')
+plt.xlabel(r'$k$', fontsize = 15)
+plt.ylabel(r'$p(k)$', fontsize = 15)
+plt.savefig(graphdir+'/14Pref', dpi=500)
 
 #%%
 '''
@@ -344,21 +412,24 @@ def Fit(N, a, b):
 
 y = []
 x = []
+Error = []
 for k in range(10,18):
     N = k
     AvgMax = np.average(DataK['MaxesN{}'.format(N)])
-    Error = np.std(DataK['MaxesN{}'.format(N)])
-    plt.errorbar(2**N, AvgMax, yerr = Error, label = 'N = 2^{}'.format(N), capsize = 3)
+    Error.append(np.std(DataK['MaxesN{}'.format(N)]))
     y.append(AvgMax)
     x.append(2**N)
     
 popt, pcov = cf(Fit, x, y)
 print('The slope is', popt[1], 'pm', np.sqrt(pcov[1][1]))
-plt.plot(x, Fit(x, *popt), '--k', label = 'Fit')
+plt.errorbar(x, y, yerr = Error, label = '$k_{1}$', capsize = 3)
+plt.plot(x, Fit(x, *popt), '--k', label = 'Two parameter fit')
 plt.yscale('log')
 plt.xscale('log')
-plt.legend()
-plt.show()
+plt.legend(fontsize = 14, loc = 'upper left')
+plt.xlabel(r'$k$', fontsize = 15)
+plt.ylabel(r'$N$', fontsize = 15)
+plt.savefig(graphdir+'/MaxKPref', dpi=500)
 
 #%%
 '''
@@ -369,11 +440,12 @@ for k in range(10,18):
     DataK['X{}Scaled'.format(N)] = [DataK['X{}'.format(N)][i]/DataK['KmaxN{}'.format(N)] for i in range(len(DataK['X{}'.format(N)]))]
     DataK['Y{}Scaled'.format(N)] = [DataK['Y{}'.format(N)][i]/DegDist(DataK['X{}'.format(N)][i], m=2) for i in range(len(DataK['X{}'.format(N)]))]
     DataK['Y{}StdevScaled'.format(N)] = [DataK['Y{}Stdev'.format(N)][i]/DegDist(DataK['X{}'.format(N)][i], m=2) for i in range(len(DataK['X{}'.format(N)]))]
-    plt.plot(DataK['X{}Scaled'.format(N)],DataK['Y{}Scaled'.format(N)], '.', label = 'M = {}'.format(N))
+    DataK[] #for scaled error
+    plt.errorbar(DataK['X{}Scaled'.format(N)],DataK['Y{}Scaled'.format(N)], yerr = , '.', label = 'N = 2^{}'.format(N))
     
 plt.yscale('log')
 plt.xscale('log')
-plt.legend()
-plt.show()
-
-
+plt.legend(fontsize = 12, loc = 'lower left')
+plt.xlabel(r'$k/k_{1}$', fontsize = 15)
+plt.ylabel(r'$p(k)/p_{\infty}(k)$', fontsize = 15)
+plt.savefig(graphdir+'/CollapsePref', dpi=500)
